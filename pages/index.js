64b96cba83ed5305ad7f724bdc56fd1fed954dd0
Dustin Myers <dustinmyers@users.noreply.github.com>
2020-06-17 19:36:49 -0600

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import { useEffect, useState, useRef } from "react";
 import Head from "next/head";
 
 const Home = () => {
-  const countDownDate = new Date("Jun 17, 2020 13:00:00 MDT").getTime();
+  const countDownDate = new Date("Jun 24, 2020 15:00:00 MDT").getTime();
   const [counter, setCounter] = useState();
 
   useInterval(() => {
